2a8f6dd7b0004ce10044e57c79d49144f494746c
Choi soomin <145344352+Choisooomin@users.noreply.github.com>
2024-08-17 18:09:29 +0900

    Title: 특정 문자의 등장 횟수, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 240817/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
+++ 240817/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
@@ -1,9 +1,16 @@
-s = input()
-ee_cnt = 0
-eb_cnt = 0
+string = input()
 
-if 'ee' in s:
-    ee_cnt+=1
-if 'eb' in s:
-    eb_cnt+=1
-print(ee_cnt,eb_cnt)
+# 문자열의 길이를 구합니다.
+leng = len(string)
+cnt1 = 0
+cnt2 = 0
+	
+# 문자열 'ee'와 'eb'가 몇 번 나왔는지 확인합니다.
+for i in range(0, leng - 1):
+	if string[i] == 'e' and string[i + 1] == 'e':
+		cnt1 += 1
+	if string[i] == 'e' and string[i + 1] == 'b':
+		cnt2 += 1
+
+# 문자열 'ee'와 'eb'가 각각 몇 번 나왔는지 출력합니다.
+print(cnt1, cnt2)
