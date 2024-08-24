95eb9b29e6fd1a2b29bd7d2b448d8e658dc2fdb8
Choi soomin <145344352+Choisooomin@users.noreply.github.com>
2024-08-24 15:16:48 +0900

    Title: 입력받아 연산하기, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240824/입력받아 연산하기/take-input-and-operate.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(); // - 기호가 나오기 전까지 입력 진행
+        int b = sc.nextInt(); // - 기호가 나오기 전까지 입력 진행
+        a+=87;
+        b%=10;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
