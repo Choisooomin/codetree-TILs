622648437f358a16502d972d61df13bbad3e02d8
Choi soomin <145344352+Choisooomin@users.noreply.github.com>
2024-08-29 17:37:59 +0900

    Title: 두 사람, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240829/두 사람/two-person.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a_age, b_age;
+        char a_gen, b_gen;
+        
+        // 입력
+        a_age = sc.nextInt();
+        a_gen = sc.next().charAt(0);
+        b_age = sc.nextInt();
+        b_gen = sc.next().charAt(0);
+
+        // 출력
+        if((a_age >= 19 && a_gen == 'M') || (b_age >= 19 && b_gen == 'M'))
+            System.out.println("1");
+        else
+            System.out.println("0");
+    }
+}
